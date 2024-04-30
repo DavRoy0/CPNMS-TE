cd2dd9ea5bba64b1102215f474cd0767cb0498aa
David <davidmatulokk@gmail.com>
2024-04-30 19:16:34 +0200

    Practica 2
    Correcto funcionamiento de la practicas 2

---
+++ CPNMS_EI1053/controllers.js
@@ -0,0 +1,180 @@
+const { graphql, buildSchema } = require('graphql')
+const Realm = require('realm')
+//import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
+
+//import { graphql, buildSchema} from 'graphql'
+
+//import model from 'model'
+const model = require('./model') //Database
+
+let DB
+
+model.getDB().then(db => {DB = db})
+
+//Notifications
+const sse  = require('./utils/notifications')
+const { forEach } = require('lodash')
+sse.start()
+
+const schema = buildSchema(`
+  type Query {
+    catastrophe: [Catastrophe]
+    insuranceCompanies: [InsuranceCompany]
+    actionProtocols: [ActionProtocol]
+    typecatastrophe: [TypeCatastrophe]
+    users: [User]
+    
+    getInfoCatastropheById(idCatastrophe: ID!): Catastrophe
+    
+    getActionProtocolsByType(typeId: ID!): [ActionProtocol]
+    
+  }
+  type Mutation{
+    createOrReplacePreferencesOfAlert(userId: ID, newProvince: String, idtypeCatastrophe: ID): Preference 
+    newCatastropheAlert(
+      country: String
+      city: String
+      province: String
+      cities: [String]
+      idtype: ID
+      area: Float
+      date: String): Catastrophe
+  }
+  type TypeCatastrophe{
+    _id: ID
+    name: String
+    description: String
+  }
+  type Catastrophe{
+    _id: ID
+    country: String
+    city: String
+    province: String
+    cities: [String]
+    type: TypeCatastrophe
+    area: Float
+    date: String
+  }
+  
+  type ActionProtocol {
+    _id: ID
+    name: String
+    description: String
+    type: TypeCatastrophe
+  }
+  type InsuranceCompany{
+    _id: ID
+    type: String
+    name: String
+    address: String
+    email: String
+  }
+  type User{
+    _id: ID,
+    address: String,
+    email: String,
+    year: String,
+    preferences: [Preference]
+  }
+  type Preference{
+    _id: ID,
+    typeCatastrophe: TypeCatastrophe,
+    province: String
+  }
+`)
+
+
+const rootValue = {
+    catastrophe: () => DB.objects('Catastrophe'),
+    typecatastrophe: () => DB.objects('TypeCatastrophe'),
+    users: () => DB.objects('User'),
+    insuranceCompanies: () => DB.objects('InsuranceCompany'),
+    actionProtocols: () => DB.objects('ActionProtocol'),
+
+
+    getInfoCatastropheById: ({ idCatastrophe }) => {
+      const objectID = new Realm.BSON.ObjectId(idCatastrophe)
+      const catastrophe = DB.objectForPrimaryKey('Catastrophe', objectID)
+      // Verifica si se encontró la catástrofe
+      if (!catastrophe) {
+          throw new Error('Catástrofe no encontrada');
+      }
+      console.log(catastrophe)
+      // Devuelve la información de la catástrofe encontrada
+      return catastrophe;
+    },  
+    getActionProtocolsByType: ({ typeId }) => {
+      const objectID = new Realm.BSON.ObjectId(typeId);
+      console.log(objectID);
+      console.log("--------------------");
+      return DB.objects('ActionProtocol').filter(protocol => {
+          console.log(protocol.type._id);
+          return protocol.type._id.equals(objectID); // Compara los ObjectIds
+      });
+
+    },
+    newCatastropheAlert: ({ country,city, province, cities, idtype, area, date }) => {
+      
+      let newCatastrophe = null;
+      const objectID= new Realm.BSON.ObjectId(idtype)
+      // Crear un nuevo objeto de catástrofe con los datos proporcionados
+      let data = {
+          _id: new Realm.BSON.ObjectId(),
+          country: country,
+          city: city,
+          province: province,
+          cities: cities,
+          type: DB.objectForPrimaryKey('TypeCatastrophe', objectID), // Obtener el objeto de tipo de catástrofe por su ID
+          area: area,
+          date: date
+      };
+      console.log(data)
+      // Escribir el nuevo objeto de catástrofe en la base de datos
+      DB.write(() => {
+          newCatastrophe = DB.create('Catastrophe', data);
+      });
+  
+      // Emitir un evento de nueva catástrofe utilizando SSE
+      sse.emitter.emit('new-catastrophe', newCatastrophe);
+  
+      return newCatastrophe;
+    },
+    createOrReplacePreferencesOfAlert: ({userId, newProvince, idtypeCatastrophe}) => {
+      let newPreference = null;
+      //comprobar si existe en ese user dicha preferencia
+      const userObjectId = new Realm.BSON.ObjectId(userId)
+      const typeObjectId = new Realm.BSON.ObjectId(idtypeCatastrophe)
+      var usuario = DB.objectForPrimaryKey('User', userObjectId)
+      console.log(usuario)
+      var listaDePreferences = usuario.preferences
+      var existePrefrence = false
+      
+      listaDePreferences.forEach(element => {
+        if((element.province == newProvince && element.typeCatastrophe)){
+          existePrefrence=true
+        }
+      })
+      
+      if(!existePrefrence){
+        
+        let data = {
+          _id: new Realm.BSON.ObjectId(),
+          typeCatastrophe: DB.objectForPrimaryKey('TypeCatastrophe', typeObjectId),
+          province: newProvince
+        }
+        
+        DB.write(() => {
+          newPreference = DB.create('Preference', data);
+          usuario.preferences.push(newPreference)
+          
+        })
+
+      }
+      return newPreference;
+      
+    }
+}
+
+exports.root   = rootValue
+exports.schema = schema
+exports.sse    = sse
---
+++ CPNMS_EI1053/model.js
@@ -0,0 +1,187 @@
+const Realm = require('realm')
+const app = new Realm.App({id: "cpmns-ikonb"})
+
+
+let TypeCatastropheSchema = {
+  name: 'TypeCatastrophe',
+  primaryKey: '_id',
+  properties: {
+    _id: 'objectId',
+     name: 'string',
+     description: 'string',
+  }
+}
+
+let CatastropheSchema = {
+  name: 'Catastrophe',
+  primaryKey: '_id',
+  properties: {
+    _id: 'objectId',
+     country: 'string',
+     city: 'string',
+     province: 'string',
+     cities: 'string[]', // Array de strings para las ciudades
+     type: 'TypeCatastrophe', // Objeto de la clase TypeCatastrophe
+     area: 'float',
+     date: 'string'
+  }
+}
+
+
+let ActionProtocolSchema = {
+  name: 'ActionProtocol',
+  primaryKey: '_id',
+  properties: {
+    _id: 'objectId',
+     name: 'string',
+     type: 'TypeCatastrophe',
+     description: 'string',
+  }
+}
+let InsuranceCompanySchema = {
+  name: 'InsuranceCompany',
+  primaryKey: '_id',
+  properties: {
+    _id: 'objectId',
+     type: 'string',
+     name: 'string',
+     address: 'string',
+     email: 'string'
+  }
+}
+let UserSchema = {
+  name: 'User',
+  primaryKey: '_id',
+  properties: {
+    _id: 'objectId',
+    address: 'string',
+    email: 'string',
+    year: 'string',
+    preferences: 'Preference[]'
+  }
+}
+let PreferenceSchema= {
+  name: 'Preference',
+  primaryKey: '_id',
+  properties:{
+    _id: 'objectId',
+    typeCatastrophe: 'TypeCatastrophe',
+    province: 'string'
+  }
+}
+// // // MODULE EXPORTS
+let sync = {
+  user: app.currentUser,
+  flexible: true,
+  initialSubscriptions: { //subconjunto de datos que se van a sincronizar
+    update: (subs, realm) => {
+          subs.add(realm.objects('TypeCatastrophe'),{name:"typeCatastrophes"})
+          subs.add(realm.objects('Catastrophe'),{name:"catastrophes"})
+          subs.add(realm.objects('ActionProtocol'),{name:"actionProtocols"})
+          subs.add(realm.objects('InsuranceCompany'),{name:"InsuranceCompanies"})
+          subs.add(realm.objects('User'),{name:"users"})
+          subs.add(realm.objects('Preference'),{name:"preferences"})
+          
+          
+    },
+    rerunOnOpen: true //reabrir en caso de recarga
+  }
+  }
+let config = {path: './data/cpmns.realm',sync: sync, schema: [CatastropheSchema,TypeCatastropheSchema,ActionProtocolSchema,InsuranceCompanySchema, PreferenceSchema,UserSchema]}
+
+exports.getDB = async () =>{
+  await app.logIn(new Realm.Credentials.anonymous())
+  return await Realm.open(config)
+} 
+exports.app = app
+
+exports.getDB = async () => await Realm.open(config)
+
+// // // // // 
+
+if (process.argv[1] == __filename){ //TESTING PART
+
+  if (process.argv.includes("--create")){ //crear la BD
+      
+      Realm.deleteFile({path: './data/cpmns.realm'}) //borramos base de datos si existe
+      console.log("prueba")
+      app.logIn(Realm.Credentials.anonymous()).then(() =>{
+        let DB = new Realm(config)
+
+      DB.write(() => {
+        let typeCatastrophe1 = DB.create('TypeCatastrophe', {
+                                          _id: Realm.BSON.ObjectId(),
+                                          name: 'Terremoto',
+                                          description: 'Un movimiento brusco de la Tierra causado por la liberación de energía acumulada debido a tensiones geológicas.'});
+        let typeCatastrophe2 = DB.create('TypeCatastrophe', {
+                                          _id: Realm.BSON.ObjectId(),
+                                          name: 'Tsunami',
+                                          description: 'Un tsunami es una serie de olas marinas generadas por un evento perturbador' });
+        let catastrophe = DB.create('Catastrophe', {
+                                _id: Realm.BSON.ObjectId(),
+                                country: 'España',
+                                city: 'Castellón de la Plana',
+                                province: 'Castellón',
+                                cities: ['Castellón de la Plana', 'Villarreal', 'Burriana'],
+                                type: typeCatastrophe1, // Referenciamos el objeto TypeCatastrophe creado previamente
+                                area: 120.5,
+                                date: '2024-03-11T18:02:59+01:00' });// La fecha y hora actual
+
+        let actionProtocol = DB.create('ActionProtocol', {
+                                _id: Realm.BSON.ObjectId(),
+                                name: 'Protocolo1',
+                                type: typeCatastrophe1, // Referenciamos el objeto TypeCatastrophe creado previamente
+                                description: 'Descripción Test1' } );
+
+        let actionProtocol2 = DB.create('ActionProtocol', {
+                                _id: Realm.BSON.ObjectId(), // ID único del nuevo protocolo
+                                name: 'Nuevo Protocolo', // Nombre del nuevo protocolo
+                                type: typeCatastrophe1, // Tipo de catástrofe asociado al nuevo protocolo
+                                description: 'Descripción del nuevo protocolo' // Descripción del nuevo protocolo
+                                });
+                                
+         let actionProtocol3 = DB.create('ActionProtocol', {
+                                _id: Realm.BSON.ObjectId(), // ID único del nuevo protocolo
+                                name: 'Nuevo Protocolo', // Nombre del nuevo protocolo
+                                type: typeCatastrophe2, // Tipo de catástrofe asociado al nuevo protocolo
+                                description: 'Descripción del nuevo protocolo' // Descripción del nuevo protocolo
+                                });
+                              
+        let insuranceCompany = DB.create('InsuranceCompany', {
+                                _id: Realm.BSON.ObjectId(),
+                                type: 'Tipo de compañía', // Asigna el tipo de compañía adecuado
+                                name: 'Nombre de la compañía',
+                                address: 'Dirección de la compañía',
+                                email: 'correo@compania.com' });
+        let preferenceSchema = DB.create('Preference',{
+                                _id: Realm.BSON.ObjectId(),
+                                typeCatastrophe: typeCatastrophe1,
+                                province: 'Castellón'
+        });
+                              
+        let userSchema = DB.create('User', {
+                                _id: Realm.BSON.ObjectId(),
+                                address: 'Paseo de la Universidad',
+                                email: 'correo@ejemplo.com' ,
+                                year: '2000',
+                                preferences: [preferenceSchema]
+        })
+        
+        console.log('Inserted objects', typeCatastrophe1, catastrophe, actionProtocol, actionProtocol2,actionProtocol3,insuranceCompany, userSchema,preferenceSchema)
+      })
+      DB.close()
+      })
+      
+      
+      //process.exit()
+  }
+  else { //consultar la BD
+
+      Realm.open({ path: './data/cpmns.realm' , schema: [CatastropheSchema,TypeCatastropheSchema,ActionProtocolSchema,InsuranceCompanySchema, UserSchema] }).then(DB => {
+        let users = DB.objects('Catastrophe')
+        users.forEach(x => console.log(x.country))
+        DB.close()
+        process.exit()
+      })
+  }
+}
